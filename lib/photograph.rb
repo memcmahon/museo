b3d95a2b83609dea7042c980b06aa88e5bd74af5
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-01-04 17:34:26 -0700

    Updates with iteration 1 functionality
    Includes photograph#id, #name, #artist_id, #museum_id, #year

--- lib/photograph.rb
+++ lib/photograph.rb
@@ -1,2 +1,15 @@
 class Photograph
+  attr_reader :id,
+              :name,
+              :artist_id,
+              :museum_id,
+              :year
+
+  def initialize(info)
+    @id        = info[:id]
+    @name      = info[:name]
+    @artist_id = info[:artist_id]
+    @museum_id = info[:museum_id]
+    @year      = info[:year]
+  end
 end
--- test/photograph_test.rb
+++ test/photograph_test.rb
@@ -4,4 +4,13 @@ require 'minitest/pride'
 require './lib/photograph'
 
 class PhotographTest < Minitest::Test
+  def test_it_is_created_with_basic_read_methods
+    photograph = Photograph.new({id: 1, name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
+
+    assert_equal 1, photograph.id
+    assert_equal 4, photograph.artist_id
+    assert_equal 2, photograph.museum_id
+    assert_equal "Rue Mouffetard, Paris (Boy with Bottles)", photograph.name
+    assert_equal 1954, photograph.year
+  end
 end
